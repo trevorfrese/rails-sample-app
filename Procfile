d81b125277000e90e07d4bc2c173ebbc43ded73e
Trevor <trevor@ubuntu.(none)>
2015-01-21 18:33:17 -0800

    chapter 8

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec unicorn -p $8080 -c ./config/unicorn.rb
+web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   	user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	@user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
