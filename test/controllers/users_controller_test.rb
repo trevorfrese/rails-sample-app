a056bc22ee3b1473c86800cbdef8fb9d347c9afe
Trevor <trevor@ubuntu.(none)>
2015-01-22 21:13:14 -0800

    chapter 9

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -36,5 +36,27 @@ class UsersControllerTest < ActionController::TestCase
     assert flash.empty?
     assert_redirected_to root_url
   end
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password:              "foobar",
+                                            password_confirmation: "foobar",
+                                            admin: 0 }
+    assert @user.admin?
+  end
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
 
 end
